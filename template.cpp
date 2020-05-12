f1c10f264dfd2dc58bc096b1bd430217979d0b8c
CJ <chaejunlee@knu.ac.kr>
2020-05-12 17:58:11 +0900

    template

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define pi pair<int, int>
+#define ll long long
+#define PQ priority_queue
+using namespace std;
+
+int t = 0, N = 0, M = 0, K = 0;
+
+void solve() {
+
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
