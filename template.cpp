499ac5431f519029ef0057a8860929dc2d7c520a
CJ <chaejunlee@knu.ac.kr>
2020-05-17 23:34:15 +0900

    20/05/17

--- template.cpp
+++ template.cpp
@@ -4,6 +4,20 @@
 #define ll long long
 #define PQ priority_queue
 using namespace std;
+bool smaller_second(pi a, pi b) {
+    if (a.second == b.second)
+        return a.first < b.first;
+    return a.second < b.second;
+}
+bool bigger_second(pi a, pi b) {
+    if (a.second == b.second)
+        return a.first > b.first;
+    return a.second > b.second;
+}
+
+// cmp
+// going up: small < bigger (if small is small, then it comes faster)
+// going down: small > bigger
 
 int t = 0, N = 0, M = 0, K = 0;
 
